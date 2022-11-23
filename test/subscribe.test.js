b1beafa5615e0f7e441b4fe49c76a44630ff5e15
killagu <killa123@126.com>
2022-11-23 15:41:37 +0800

    fix: should not emit timeout when has cache

--- test/subscribe.test.js
+++ test/subscribe.test.js
@@ -44,5 +44,46 @@ describe('test/subscrib.test.js', () => {
       await follower.close();
       await leader.close();
     });
+
+    it('should not timeout when already have data', async () => {
+      const leader = new ApiClient({
+        singleMode: false,
+        isLeader: true,
+      });
+      const follower = new ApiClient({
+        clusterOptions: {
+          subscribeTimeout: 1000,
+        },
+        singleMode: false,
+        isLeader: false,
+      });
+      await follower.ready();
+      const errors = [];
+      const values = [];
+      follower.on('error', err => {
+        errors.push(err);
+      });
+      // ensure has data
+      follower.subscribe({
+        key: 'timeout:500',
+      }, value => {
+        values.push(value);
+      });
+      await sleep(1000);
+      // second subscribe
+      follower.subscribe({
+        key: 'timeout:600',
+      }, value => {
+        values.push(value);
+      });
+      await sleep(1000);
+      assert.deepStrictEqual(values, [
+        'hello:500',
+        'hello:600',
+      ]);
+      assert(errors.length === 0);
+      await follower.close();
+      await leader.close();
+    });
   });
 });
